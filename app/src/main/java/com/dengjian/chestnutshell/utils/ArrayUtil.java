3f084e8e316d045f50cf872ce1bcf3ec49550236
DreamCloudWalker <yashanhanhun@qq.com>
2019-07-27 17:58:57 +0800

    [Add] add ArrayUtil

--- app/src/main/java/com/dengjian/chestnutshell/utils/ArrayUtil.java
+++ app/src/main/java/com/dengjian/chestnutshell/utils/ArrayUtil.java
@@ -1,5 +1,28 @@
 package com.dengjian.chestnutshell.utils;
 
+import java.lang.reflect.Array;
+
 public class ArrayUtil {
 
+    /**
+     * 合并数组
+     * @param arrayLhs 前数组（插队数组）
+     * @param arrayRhs 后数组（已有数组）
+     * @return
+     */
+    public static Object combineArray(Object arrayLhs, Object arrayRhs) {
+        Class<?> localClass = arrayLhs.getClass().getComponentType();
+        int oldLength = Array.getLength(arrayLhs);
+        int newLength = oldLength + Array.getLength(arrayRhs);
+        Object result = Array.newInstance(localClass, newLength);   // 生成返回的数组对象
+        for (int i = 0; i < newLength; ++i) {
+            if (i < oldLength) {
+                Array.set(result, i, Array.get(arrayLhs, i));
+            } else {
+                Array.set(result, i, Array.get(arrayRhs, i - oldLength));
+            }
+        }
+
+        return result;
+    }
 }
